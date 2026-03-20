4ec74c4612658e56c975c8dfc12d6290a764d09d
Tigge Nilsson <tigge.nilsson@ericsson.com>
2026-03-20 21:31:26 +0100

    Match portfolio color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Departures from "./components/Departures/Departures";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <h1>Home Dashboard</h1>
       <Departures />
     </div>
